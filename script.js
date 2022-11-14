213b543389fa55e45fa9cec942cef66e43da1873
unknown <nab_s_a@protonmail.com>
2022-11-14 20:36:53 +0100

    updating layout

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+/*const runProjectLocal = document.getElementById("run-project-local");
+const techStack = document.getElementById("tech-stack");
+*/
+/*const dropDown = function (event) {
+    event.target.style.
+}
+*/
